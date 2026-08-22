b02f1c126609aeec6424779abd02f4ccb2f186f4
Pratham Jain <jainpratham370@gmail.com>
2026-08-22 11:50:53 +0530

    [162] [Time Beats: 100.00%] [Memory Beats: 88.41%] - LeetPush

---
+++ 162-find-peak-element/162-Find-Peak-Element.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(mid-1==-1 && mid+1==nums.length){
+                return 0;
+            }
+            else if(mid-1==-1){
+                if(nums[mid]>nums[mid+1])return mid;
+                else low=mid+1;
+            }
+            else if(mid+1==nums.length){
+                if(nums[mid]>nums[mid-1])return mid;
+                else high=mid-1;
+            }
+            else{
+                if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])return mid;
+                else if(nums[mid+1]>nums[mid])low=mid+1;
+                else high=mid-1;
+            }
+        }
+        return -1;
+    }
+}
