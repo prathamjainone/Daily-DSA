73cfd08ce7339a77bcac212111c8951be800baa2
Pratham Jain <jainpratham370@gmail.com>
2026-08-24 04:56:42 +0530

    [435] [Time Beats: 74.88%] [Memory Beats: 77.14%] - LeetPush

---
+++ 435-non-overlapping-intervals/435-Non-overlapping-Intervals.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+         Arrays.sort(intervals,(a,b)->{
+            return a[1]-b[1];
+        });
+
+        int lastend=intervals[0][1];
+        int count=0;
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]<lastend){
+                count++;
+            }
+            else{
+                lastend=intervals[i][1];
+            }
+        }
+        return count;
+    }
+}
