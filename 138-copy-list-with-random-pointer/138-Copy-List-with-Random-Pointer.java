6ef60dc45a3e2cc1b8c9452d5081e41e23227767
Pratham Jain <jainpratham370@gmail.com>
2026-08-23 14:17:35 +0530

    [138] [Time Beats: 100.00%] [Memory Beats: 88.24%] - LeetPush

--- 138-copy-list-with-random-pointer/138-Copy-List-with-Random-Pointer.java
+++ 138-copy-list-with-random-pointer/138-Copy-List-with-Random-Pointer.java
@@ -13,27 +13,64 @@ class Node {
 }
 */
 
+// class Solution {
+//     public Node copyRandomList(Node head) {
+//         HashMap<Node,Node>map=new HashMap<>();
+//         Node dummy=new Node(-1);
+//         Node neww=dummy;
+//         Node temp=head;
+//         while(temp!=null){
+//             dummy.next=new Node(temp.val);
+//             dummy=dummy.next;
+//             map.put(temp,dummy);
+//             temp=temp.next;
+//         }
+//         temp=head;
+//         dummy=neww.next;
+//         while(temp!=null){
+//             if(temp.random!=null){
+//                 dummy.random=map.get(temp.random);
+//             }
+//             dummy=dummy.next;
+//             temp=temp.next;
+//         }
+//         return neww.next;
+//     }
+// }
+
 class Solution {
     public Node copyRandomList(Node head) {
-        HashMap<Node,Node>map=new HashMap<>();
-        Node dummy=new Node(-1);
-        Node neww=dummy;
-        Node temp=head;
-        while(temp!=null){
-            dummy.next=new Node(temp.val);
-            dummy=dummy.next;
-            map.put(temp,dummy);
-            temp=temp.next;
+        Node dummy = new Node(-1);
+        Node temp = dummy;
+        Node t = head;
+        Node y = head;
+        while (head != null) {
+            Node front = head.next;
+            Node clone = new Node(head.val);
+
+            head.next = clone;
+            clone.next = front;
+
+            head = front;
         }
-        temp=head;
-        dummy=neww.next;
-        while(temp!=null){
-            if(temp.random!=null){
-                dummy.random=map.get(temp.random);
+        while (t != null) {
+            if (t.random != null) {
+                t.next.random = t.random.next;
             }
-            dummy=dummy.next;
-            temp=temp.next;
+            t = t.next.next;
         }
-        return neww.next;
+        t = y;
+        while (t != null) {
+            Node front = t.next.next; // Next original (B)
+            Node clone = t.next; // Current clone (A')
+
+            temp.next = clone; // Build new list
+            temp = temp.next;
+
+            t.next = front; // Restore original list (A -> B)
+            t = front; // Advance
+        }
+
+        return dummy.next;
     }
 }
