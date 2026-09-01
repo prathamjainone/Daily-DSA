7d95d4a5f47132159cbce53d1e03b4e8e76e214e
Pratham Jain <jainpratham370@gmail.com>
2026-09-01 16:38:11 +0530

    [215] [Time Beats: 97.93%] [Memory Beats: 11.08%] - LeetPush

--- 215-kth-largest-element-in-an-array/215-Kth-Largest-Element-in-an-Array.java
+++ 215-kth-largest-element-in-an-array/215-Kth-Largest-Element-in-an-Array.java
@@ -1,17 +1,40 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer>pq=new PriorityQueue<>();
+        return qs(nums,k,0,nums.length-1);
+    }
 
-        for(int i=0;i<k;i++){
-            pq.add(nums[i]);
+    public int qs(int[]nums,int k,int low,int high){
+        int p=partition(nums,low,high);
+        if(p==k-1)return nums[p];
+        else if(p>k-1){
+            return qs(nums,k,low,p-1);
         }
+        return qs(nums,k,p+1,high);
+        
+    }
 
-        for(int i=k;i<nums.length;i++){
-            if(pq.peek()<nums[i]){
-                pq.poll();
-                pq.offer(nums[i]);
+    public int partition(int[]nums,int low,int high){
+        int p=(int)(Math.random()*(high-low+1))+low;
+        int temp=nums[p];
+        nums[p]=nums[low];
+        nums[low]=temp;
+
+        int l=low+1;
+        int r=high;
+        while(l<=r){
+            if(nums[l]<nums[low] && nums[r]>nums[low]){
+                temp=nums[r];
+                nums[r]=nums[l];
+                nums[l]=temp;
+                l++;
+                r--;
             }
+            if(nums[l]>=nums[low])l++;
+            if(nums[r]<=nums[low])r--;
         }
-        return pq.peek();
+        temp=nums[r];
+        nums[r]=nums[low];
+        nums[low]=temp;
+        return r;
     }
 }
