46d76018660b2b72af876e2fe0eca03a5f0d0a2b
Pratham Jain <jainpratham370@gmail.com>
2026-09-05 12:31:50 +0530

    [257] [Time Beats: 73.04%] [Memory Beats: 9.42%] - LeetPush

--- 257-binary-tree-paths/257-Binary-Tree-Paths.java
+++ 257-binary-tree-paths/257-Binary-Tree-Paths.java
@@ -16,6 +16,7 @@
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String>ans=new ArrayList<>();
+        if (root == null) return ans;
         String s=String.valueOf(root.val);
         if(root.left==null && root.right==null){
             ans.add(s);
