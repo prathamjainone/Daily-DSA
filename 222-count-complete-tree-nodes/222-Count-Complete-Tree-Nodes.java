15b2be082aeee1cb0fa62d5b881d4173ce527698
Pratham Jain <jainpratham370@gmail.com>
2026-09-08 12:13:23 +0530

    [222] [Time Beats: 100.00%] [Memory Beats: 11.80%] - LeetPush

---
+++ 222-count-complete-tree-nodes/222-Count-Complete-Tree-Nodes.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root==null)return 0;
+        int lh=left(root);
+        int rh=right(root);
+        if(lh==rh)return (1<<lh)-1;
+        return 1+countNodes(root.left)+countNodes(root.right);
+    }
+
+     private int left(TreeNode node) {
+        int height = 0;
+        while (node != null) {
+            height++;
+            node = node.left;
+        }
+        return height;
+    }
+    private int right(TreeNode node) {
+        int height = 0;
+        while (node != null) {
+            height++;
+            node = node.right;
+        }
+        return height;
+    }
+}
